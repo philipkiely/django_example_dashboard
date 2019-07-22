2e3e0c6eadc1856d9ed9974f4a3a8dde82bcdbe5
Philip Kiely <kielyphi@grinnell.edu>
2019-07-22 07:13:04 -0400

    autentication done

---
+++ README.md
@@ -0,0 +1,3 @@
+### Django website example: login-protected dashboard
+
+This repository is example code for an article on mkdev. URL coming soon.
---
+++ dashboard/core/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Index(LoginRequiredMixin, View):
+    template = 'index.html'
+    login_url = '/login/'
+    
+    def get(self, request):
+        return render(request, self.template)
+
+
+class Login(View):
+    template = 'login.html'
+    
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.template, {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, self.template, {'form': form})
